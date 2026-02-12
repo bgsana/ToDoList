a58e436493c015917c21f5a59f4061350695d96c
bgsana <bgs.ana08@gmail.com>
2026-02-11 21:25:35 -0300

    Initial project structure setup

--- ToDoList/Data/AppDbContext.cs
+++ ToDoList/Data/AppDbContext.cs
@@ -5,8 +5,7 @@ namespace ToDoList.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options)
-    : base(options) { }
+    public AppDbContext(DbContextOptions options) : base(options) { }
 
     //Informar a model para o nosso contexto e como ela irá ser criada ao executar as migrations
     public DbSet<Tarefa> Tarefas { get; set; }
